e142d6f9ad3035b77916efc27b5dcb2907835cf7
jiongjiongli <33146359+jiongjiongli@users.noreply.github.com>
2025-04-06 20:20:10 -0500

    Add model service.

--- frontend/swagger_server/controllers/table_extract_controller.py
+++ frontend/swagger_server/controllers/table_extract_controller.py
@@ -1,18 +1,56 @@
 import connexion
+from pathlib import Path
 import six
+import sys
+sys.path.append("/home/cc/jiongjiong/proj/git/table-transformer/src")
 
-from swagger_server import util
 from flask import send_file, request as flask_request
 
+from swagger_server import util
+from model_service import get_args, TableExtractModel
 
-def table_extract_post(file=None):
+
+def create_model():
+    dir_name_postfix = "service"
+    input_args = [
+        '--mode', 'extract',
+        '--image_dir', f'/home/cc/jiongjiong/data/processed_tiny_fintabnet/page_images/{dir_name_postfix}',
+        '--words_dir', f'/home/cc/jiongjiong/data/processed_tiny_fintabnet/ocr_results/{dir_name_postfix}',
+        '--out_dir', f'/home/cc/jiongjiong/inference_output_{dir_name_postfix}',
+        '--structure_config_path', 'structure_config.json',
+        '--structure_model_path', '/home/cc/jiongjiong/models/TATR-v1.1-Fin-msft.pth',
+        '--detection_config_path', 'detection_config.json',
+        '--detection_model_path', '/home/cc/jiongjiong/models/pubtables1m_detection_detr_r18.pth',
+        '--html',
+        '--csv',
+        '--visualize'
+    ]
+
+    args = get_args(input_args)
+    model = TableExtractModel(args)
+    return model
+
+
+model = create_model()
+
+
+def table_extract_post(file):
+    args = model.args
+
     file_name = file.filename
     # file_data = file.read()
-    file.save(f"temp_{file_name}")
+    image_file_path = Path(args.image_dir) / file.filename
+    image_file_path.parent.mkdir(parents=True, exist_ok=True)
+    file.save(str(image_file_path))
+
     query_params = connexion.request.query_params
 
     for query, value in query_params.items():
         print(query, value)
+
+    model.process(str(image_file_path))
+
+    print(args.out_dir)
 
     output_file_path = "lab01_dataset_2.csv"
     return send_file(output_file_path,
